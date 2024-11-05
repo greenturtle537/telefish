e7fc07ac92a498f883de3baef46391aa62948c73
greenturtle537 <support@glitchtech.top>
2024-11-05 22:08:52 +0000

    Add graphic loader

--- telefish.js
+++ telefish.js
@@ -4,11 +4,36 @@ const author = "greenturtle537";
 const REVISION = "$Revision: 0.1 $".split(' ')[1];
 const tear_line = "\r\n--- " + js.exec_file + " " + REVISION + "\r\n";
 
+const test = js.exec_dir + "test.bin";
+
+function show_image(filename, fx, delay)
+{
+	var dir = directory(filename);
+	filename = dir[random(dir.length)];
+	var Graphic = load({}, "graphic.js");
+	var sauce_lib = load({}, "sauce_lib.js");
+	var sauce = sauce_lib.read(filename);
+	if(delay === undefined)
+		delay = options.image_delay;
+	if(sauce && sauce.datatype == sauce_lib.defs.datatype.bin) {
+		try {
+			var graphic = new Graphic(sauce.cols, sauce.rows);
+			graphic.load(filename);
+			if(fx && graphic.revision >= 1.82)
+				graphic.drawfx('center', 'center');
+			else
+				graphic.draw('center', 'center');
+			sleep(delay);
+		} catch(e) { 
+			log(LOG_DEBUG, e);
+		}
+	}
+}
 
 try {
 
 	console.print("The game is still being built. Please wait. It's 'Trouta be fire' ");
-	console.beep();
+	
 	console.pause();
 	exit(0);
 
