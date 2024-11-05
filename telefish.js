f059c13e9522f353ccaaee01571134911ecad5b6
greenturtle537 <support@glitchtech.top>
2024-11-05 22:24:06 +0000

    Fix

--- telefish.js
+++ telefish.js
@@ -1,3 +1,35 @@
+// Consts
+const title = "Telefish";
+const author = "greenturtle537";
+const REVISION = "$Revision: 0.1 $".split(' ')[1];
+const tear_line = "\r\n--- " + js.exec_file + " " + REVISION + "\r\n";
+
+const test = js.exec_dir + "test.bin";
+
+function show_image(filename, fx, delay)
+{
+	var dir = directory(filename);
+	filename = dir[random(dir.length)];
+	var Graphic = load({}, "graphic.js");
+	var sauce_lib = load({}, "sauce_lib.js");
+	var sauce = sauce_lib.read(filename);
+	if(delay === undefined)
+		delay = options.image_delay;
+	if(sauce && sauce.datatype == sauce_lib.defs.datatype.bin) {
+		try {
+			var graphic = new Graphic(sauce.cols, sauce.rows);
+			graphic.load(filename);
+			if(fx && graphic.revision >= 1.82)
+				graphic.drawfx('center', 'center');
+			else
+				graphic.draw('center', 'center');
+			sleep(delay);
+		} catch(e) { 
+			log(LOG_DEBUG, e);
+		}
+	}
+}
+
 function gameLoop() {
 	var gridSize = 16;
 	var grid = [];
@@ -30,9 +62,8 @@ function gameLoop() {
 			console.crlf();
 		}
 
-		// Get input using console.getkey
-		var key = console.getkey(K_NONE);
-
+		// Get input
+		var key = console.getkey(K_NOECHO);
 		if (key) {
 			switch (key) {
 				case KEY_UP:
@@ -55,3 +86,35 @@ function gameLoop() {
 	}
 	console.clear();
 }
+
+try {
+
+	console.print("The game is still being built. Please wait. It's 'Trouta be fire' ");
+	gameLoop();
+	console.pause();
+	exit(0);
+
+} catch(e) {
+	
+	var msg = file_getname(e.fileName) + 
+		" line " + e.lineNumber + 
+		": " + e.message;
+	if(js.global.console) {
+		console.crlf();
+	}
+	alert(msg);
+	if(user.alias != author) {
+		var msgbase = new MsgBase('mail');
+		var hdr = { 
+			to: author,
+			from: user.alias || system.operator,
+			subject: title
+		};
+		msg += tear_line;
+		if(!msgbase.save_msg(hdr, msg)) {
+			alert("Error saving exception-message to: " + options.sub);
+		}
+		msgbase.close();
+	}
+}
+
