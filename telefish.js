a93a935aa57bd62ee123fab4b795d80fda0644a5
greenturtle537 <support@glitchtech.top>
2024-11-08 19:31:09 +0000

    Add function to retrieve character at specific grid position and update grid width to 80

--- telefish.js
+++ telefish.js
@@ -37,8 +37,20 @@ function show_image(filename, fx, delay)
 	}
 }
 
+function getCharAtPos(x, y) {
+	// Move the cursor to position (x, y)
+	console.gotoxy(x, y);
+
+	// Calculate the index in the screen buffer
+	var index = (y - 1) * console.screen_columns + (x - 1);
+
+	// Get the character at that position
+	var charAtPosition = console.screen_buf[index];
+	return charAtPosition;
+}
+
 function gameLoop() {
-	var gridWidth = 48;
+	var gridWidth = 80;
 	var gridHeight = 24;
 	var grid = [];
 	var playerX = Math.floor(gridWidth / 2);
