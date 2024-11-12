25ee8cab3ce36f088705b12ab25a45a2deaeef7e
greenturtle537 <support@glitchtech.top>
2024-11-12 02:34:34 +0000

    Refactor gameLoop to acknowledge nodes only if they are not duplicates

--- telefish.js
+++ telefish.js
@@ -431,8 +431,8 @@ function gameLoop() {
 		for(var i = 0; i < messages.length; i=i+3) {
 			if (!(messages[i] === '' || messages[i] === null)) {
 				if (messages[i] === "\x7fDISCOVER\x7f") {
-					broadcastAcknowledge(messages[i-1]);
 					if (!(checkDuplicateNode(node))) {
+						broadcastAcknowledge(messages[i-1]);
 						nodesOnline.push(messages[i-1]);
 					}
 				} else {
