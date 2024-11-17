4b492ae9403187cd5df021b179608486b52432fc
greenturtle537 <support@glitchtech.top>
2024-11-17 19:32:22 +0000

    Add drawTypedMessage method to MessageWindow for formatted message display

--- MessageWindow.js
+++ MessageWindow.js
@@ -22,6 +22,55 @@ MessageWindow.prototype.drawMessages = function() {
     }
 };
 
+MessageWindow.prototype.drawTypedMessage = function(user, message) {
+    var maxWidth = this.width - 2;
+    var formattedMessage = user + ": " + message;
+    var words = formattedMessage.split(' ');
+    var lines = [];
+    var line = '';
+
+    for (var i = 0; i < words.length; i++) {
+        var word = words[i];
+
+        while (word.length > maxWidth) {
+            var part = word.substring(0, maxWidth);
+            word = word.substring(maxWidth);
+            if (line.length > 0) {
+                lines.push(line);
+                line = '';
+            }
+            lines.push(part);
+        }
+
+        if (line.length + word.length + (line.length > 0 ? 1 : 0) > maxWidth) {
+            lines.push(line);
+            line = word;
+        } else {
+            if (line.length > 0) line += ' ';
+            line += word;
+        }
+    }
+    if (line.length > 0) lines.push(line);
+
+    var startLine = this.y + this.height - 1 - lines.length;
+    for (var i = 0; i < lines.length; i++) {
+        console.gotoxy(this.x + 1, startLine + i);
+        for (var x = 1; x < this.width - 1; x++) {
+            console.print(' ');
+        }
+        console.gotoxy(this.x + 1, startLine + i);
+        console.print(lines[i]);
+    }
+
+    var yPosition = this.y + this.height - lines.length - 2;
+    console.gotoxy(this.x + 1, yPosition);
+    for (var x = 1; x < this.width - 1; x++) {
+        console.print('-');
+    }
+    return lines.length;
+};
+
+
 MessageWindow.prototype.display = function() {
     this.draw();
     this.drawTitle(this.title);
--- telefish.js
+++ telefish.js
@@ -32,12 +32,13 @@ var typeToggled = false;
 
 // Telefish global variables
 var Window = load({}, "window.js");
-var chatWindow = new Window(30, 24, 1, 1);
+var MessageWindow = load({}, "MessageWindow.js");
+
+var chatWindow = new MessageWindow(30, 24, 1, 1);
 chatWindow.setTitle("====Telefish  Node  Chat====");
 var fishWindow = new Window(40, 10, 35, 10);
 fishWindow.setTitle("=Currently waiting for a fish to bite=");
 
-var MessageWindow = load({}, "MessageWindow.js");
 
 
 // TODO: Rename to messages
