86e3b15bcd1e2ac1d8c5dbdc8f382c42e683bffd
greenturtle537 <support@glitchtech.top>
2024-11-17 23:20:04 +0000

    Refactor NodeTalk to use 'this' context for userNode and nodesOnline properties

--- NodeTalk.js
+++ NodeTalk.js
@@ -1,14 +1,14 @@
 function NodeTalk() {
     this.userNode = bbs.node_num;
     this.currentUser = new User(bbs.node_useron);
-    this.telefish = currentUser.curxtrn // For ref, this is currently telefish but may change
+    this.telefish = this.currentUser.curxtrn // For ref, this is currently telefish but may change
 
     this.nodesOnline = [];
 };
 
 NodeTalk.prototype.probeNode = function (node) {
 	var targetNode = new User(node);
-	if (targetNode.curxtrn === telefish) {
+	if (targetNode.curxtrn === this.telefish) {
 		return true;
 	} else {
 		//nodesOnline.splice(nodesOnline.indexOf(node), 1);
@@ -20,14 +20,14 @@ NodeTalk.prototype.probeNode = function (node) {
 NodeTalk.prototype.broadcastDiscover = function () {
 	for(var i = 0; i < system.nodes; i++) {
 		if (nodeTalk.probeNode(i) && !checkDuplicateNode(i)) {
-			system.put_node_message(i, "\x1bTF\x1b"+userNode+"\x1b"+"\x7fDISCOVER\x7f");
+			system.put_node_message(i, "\x1bTF\x1b"+this.userNode+"\x1b"+"\x7fDISCOVER\x7f");
 		}
 	}
 };
 
 NodeTalk.prototype.checkDuplicateNode = function(node) {
-	for(var i = 0; i < nodesOnline.length; i++) {
-		if (parseInt(node) === parseInt(nodesOnline[i])) {
+	for(var i = 0; i < this.nodesOnline.length; i++) {
+		if (parseInt(node) === parseInt(this.nodesOnline[i])) {
 			return true;
 		}
 	}
@@ -39,13 +39,13 @@ NodeTalk.prototype.broadcastAcknowledge = function(node) {
 	if (checkDuplicateNode(node)) {
 		return;
 	}
-	system.put_node_message(node, "\x1bTF\x1b"+userNode+"\x1b"+"\x7fDISCOVER\x7f");
+	system.put_node_message(node, "\x1bTF\x1b"+this.userNode+"\x1b"+"\x7fDISCOVER\x7f");
 }
 
 
 NodeTalk.prototype.sendMessage = function(message, name) {
-	for(var i = 0; i < nodesOnline.length; i++) {
-		system.put_node_message(nodesOnline[i], "\x1bTF\x1b"+name+"\x1b"+message);
+	for(var i = 0; i < this.nodesOnline.length; i++) {
+		system.put_node_message(this.nodesOnline[i], "\x1bTF\x1b"+name+"\x1b"+message);
 	}
 }
 
