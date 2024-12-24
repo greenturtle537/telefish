385cb0178eb4e52c79c5791fed427a77036677e0
greenturtle537 <support@glitchtech.top>
2024-12-24 00:58:13 -0500

    Add console print for base64 code in loadGraphicsFromANSI function and fix formatting in drawGraphicAt function

--- telefish.js
+++ telefish.js
@@ -67,6 +67,7 @@ function loadGraphicsFromANSI(filename) {
 				const tile = graphic.get(x, y, x + 3, y + 1);
 				const bin = tile.BIN;
 				const base64Code = base64Chars[Math.floor(codeIndex / 64)] + base64Chars[codeIndex % 64];
+				console.print(base64Code);
 				tiles[base64Code] = bin;
 				codeIndex++;
 				if (codeIndex >= 64 * 64) {
@@ -81,7 +82,7 @@ function loadGraphicsFromANSI(filename) {
 function drawGraphicAt(x, y, base64Code, graphicsDict) {
 	const bin = graphicsDict[base64Code];
 	if (!bin) {
-		console.print("Graphic with code"+base64Code+"not found.");
+		console.print("Graphic with code "+base64Code+" not found.");
 	} else {
 		const graphic = new Graphic();
 		graphic.BIN = bin;
