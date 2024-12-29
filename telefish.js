0d1610740512e53a329368525d28f8ac3ffabca1
greenturtle537 <support@glitchtech.top>
2024-12-28 23:12:52 -0500

    Increase tile graphic height for improved rendering in loadGraphicsFromANSI function

--- telefish.js
+++ telefish.js
@@ -58,7 +58,7 @@ function loadGraphicsFromANSI(filename) {
 	var dir = directory(filename);
 	filename = dir[random(dir.length)];
 
-	var tileGraphic = new Graphic(80, 72);
+	var tileGraphic = new Graphic(80, 140);
 	tileGraphic.load(filename);
 
 	console.gotoxy(1, 25);
