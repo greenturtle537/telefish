06fcf9ed778f32c6318ae567b1b5947827c27c11
greenturtle537 <support@glitchtech.top>
2025-01-26 15:45:41 -0500

    Fix x-offset and y-offset calculations in Tilesheet draw method to use tileGraphic width

--- Tilesheet.js
+++ Tilesheet.js
@@ -28,8 +28,8 @@ Tilesheet.prototype.loadGraphicsFromANSI = function(filename, width, height) {
 Tilesheet.prototype.draw = function(x, y, base64Code) {
 	var graphicAddress = base64ToInt(base64Code);
     //alert("Drawing tile: " + graphicAddress + "?: " + Math.floor(graphicAddress / this.width));
-	var graphicxoff = Math.floor(graphicAddress % this.width);
-	var graphicyoff = Math.floor(graphicAddress / this.width);
+	var graphicxoff = Math.floor(graphicAddress % this.tileGraphic.width);
+	var graphicyoff = Math.floor(graphicAddress / this.tileGraphic.width);
 
 	this.tileGraphic.draw(
 		xpos = x,
